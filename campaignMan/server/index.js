2a425c8a7cf3280d77de14124179908c0b2c7c01
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-08-08 21:05:16 -0700

    provision a remote instance of mongo connect it

--- campaignMan/server/index.js
+++ campaignMan/server/index.js
@@ -1,8 +1,17 @@
 //Common J.S. modules to bring in express with the require key word to create a server.
 const express = require('express');
 
+//Keys to mongodb
+const keys = require('./config/keys');
+
+//Mongo atlas database
+const mongoose = require('mongoose');
+
 //Require passport just need to run the passport file. Nothing being returned.
 require('./services/passport')
+
+//Connect to mongo atlas
+mongoose.connect(keys.mongoURI)
 
 //Use a variable to hold express functionality. declaration ready to rock and roll.
 const app = express();
