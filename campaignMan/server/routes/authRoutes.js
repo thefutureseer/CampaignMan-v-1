1eaeac4954f4274e30a896784f6f2b16f59e88c8
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-08-25 00:01:10 -0700

    cookies

--- campaignMan/server/routes/authRoutes.js
+++ campaignMan/server/routes/authRoutes.js
@@ -5,15 +5,20 @@ const passport = require('passport');
 module.exports = app => {
 app.get('/auth/google', passport.authenticate('google', {
   scope: ['profile', 'email']
-})
-// ,
-// function(req, res) {
-//   res.send(reg.user)
-// }
-);
+}));
 
 app.get('/auth/google/callback', passport.authenticate('google', ));
 
+//Logout
+app.get('/api/logout', (req, res) => {
+ //Logout function attatched to the request property by passport
+ //It takes the cookie and kills the id thats in there
+ req.logout();
+ //Let frontend know logged out
+ res.send(req.user);
+});
+
+//Test auth
 app.get('/api/current_user', (req, res)=> {
   res.send(req.user);
 })
